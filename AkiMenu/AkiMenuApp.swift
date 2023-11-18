611eb8b4f1a36695d229e0a7171b343d76f1aaa8
Thong Hoang <thonghoangdanh@quanscient.com>
2023-11-18 14:58:46 +0200

    Initial Commit

---
+++ AkiMenu/AkiMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkiMenuApp.swift
+//  AkiMenu
+//
+//  Created by Thong Hoang on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AkiMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
